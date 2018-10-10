d1251b50bd0273fec1b5d9cdc1346f67657b0071
N Fourtounis <nickfortune@windowslive.com>
2018-10-10 17:42:13 +0300

    Book + ApplicationDbContext Classes

---
+++ BookList_v1/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookList_v1.Models
+{
+    public class Book
+    {
+        public int Id{ get; set; }
+        public String Name{ get; set; }
+    }
+}
