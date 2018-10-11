839416bbf1d136ff1dde16a3e24baa010e5d1705
N Fourtounis <nickfortune@windowslive.com>
2018-10-11 15:39:30 +0300

    Fixed Displayed Booknames

---
+++ BookList_v1/Migrations/20181011121952_updateDb.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookList_v1.Migrations
+{
+    public partial class updateDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
